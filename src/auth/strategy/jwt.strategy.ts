fedc49c72bd61f9dc7f36f6cd476a3b15ab89960
Papapanda-PC <rafaeljonathan.work@gmail.com>
2023-05-22 01:22:13 +0700

    add jwt strategy

---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(private readonly prisma: PrismaService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.SESSION_SECRET,
+    });
+  }
+}
