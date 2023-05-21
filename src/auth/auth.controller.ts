9f22c7a4dc2bf22623c23db6061f885a4decd686
Papapanda-PC <rafaeljonathan.work@gmail.com>
2023-05-21 23:35:17 +0700

    add register logic

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthDto } from './dto/auth.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
 
-//POST /auth/signup
-@Post ('/signup')
+  // POST /auth/signup
+  @Post('/signup')
+  async signup(@Body() input: AuthDto) {
+    return this.authService.createUser(input);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import * as argon from 'argon2';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto/auth.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async createUser(input: AuthDto) {
+    const hashedPassword = await argon.hash(input.password);
+
+    try {
+      return await this.prisma.users.create({
+        data: {
+          email: input.email,
+          password: {
+            create: {
+              userPassword: hashedPassword,
+            },
+          },
+        },
+      });
+    } catch (error) {
+      if (error.code === 'P2002') {
+        throw new ForbiddenException('Email has been used');
+      }
+      throw error;
+    }
+  }
+}
